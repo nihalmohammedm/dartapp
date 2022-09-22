5cec58cc730964523f2ccd01887872ce65a03c16
Nihal Mohammed <nihalmohammed@Nihals-MacBook-Air.local>
2022-09-22 15:29:45 +0530

    basic condition statement for password authentication

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,53 +1,77 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  LoginScreen({Key? key}) : super(key: key);
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        actions: [
-          Icon(
-            Icons.account_circle_rounded,
-            size: 35,
-            color: Colors.blue,
-          ),
-        ],
-        title: Text('Login'),
+        title: Text(
+          'Sanas App',
+          style: TextStyle(fontFamily: 'Lato'),
+        ),
       ),
-
       body: Center(
+          child: SingleChildScrollView(
         child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(30.0),
-            child: Text(
-              'welcome',
-              
-
-              style: TextStyle(fontSize: 20,color: Colors.pink ),
-              ),
-          ),Padding(padding: EdgeInsets.all(2.0),),
-          Image.network("https://us.123rf.com/450wm/rawpixel/rawpixel1603/rawpixel160308149/53554297-learn-learning-education-knowledge-wisdom-studying-concept.jpg?ver=6"),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              
-
+          children: [
+            Container(
+              margin: EdgeInsets.all(30.0),
             ),
-          ),
-          ElevatedButton(onPressed: (){}, child: Text("Search"))
- 
-        ],
+            Padding(
+              padding: EdgeInsets.all(2.0),
+            ),
+            Container(
+              height: 92.00,
+              width: 92.00,
+              child: Image.asset(
+                "assets/logincon.png",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: userNameController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Your Username',
+                    labelText: 'User Name',
+                    suffix: Icon(Icons.account_circle_rounded)),
+              ),
+            ), // Username
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Your Password',
+                  labelText: 'Password',
+                ),
+              ),
+            ), //Password
+            ElevatedButton(
+                onPressed: () {
+                  final userName = userNameController.text;
+                  print("username = $userName");
+                  final passWord = passwordController.text;
+                  print("password = $passWord");
+                  if (userName == "admin" && passWord == "12345") {
+                    print("login successfully");
+                  } else {
+                    print("Password is incorrect");
+                  }
+                },
+                child: Text("Login"))
+          ],
+        ),
       )),
-      
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: const LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
